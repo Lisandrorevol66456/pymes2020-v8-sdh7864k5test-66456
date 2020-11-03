f6bbcf09024ce123a9c62911139ca26981ff9a12
Lisandrorevol66456 <revol.lisandro@gmail.com>
2020-11-03 01:31:56 -0300

    error en servicios.component.ts

--- src/app/components/servicios/servicios.component.ts
+++ src/app/components/servicios/servicios.component.ts
@@ -1,16 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { Servicio } from "../../models/Servicio";
+import { ServiciosService } from "../../services/servicios.service";
 @Component({
-  selector: 'app-servicios',
-  templateUrl: './servicios.component.html',
-  styleUrls: ['./servicios.component.css']
+  selector: "app-servicios",
+  templateUrl: "./servicios.component.html",
+  styleUrls: ["./servicios.component.css"]
 })
 export class ServiciosComponent implements OnInit {
-  Titulo="Servicios";
-  
-  constructor() { }
+  Titulo = "servicios";
+  Servi: Servicio[] = [];
+
+  constructor(private serviciosServices: ServiciosService) {}
 
   ngOnInit() {
+    this.GetServicios();
   }
-
+  GetServicios() {
+    this.serviciosServices.get().subscribe((res: Servicio[]) => {
+      this.Servi = res;
+    });
+  }
 }
+
+// constructor(
+//     //private articulosFamiliasService:  ArticulosFamiliasService
+//     private articulosFamiliasService:  MockArticulosFamiliasService
+//   ){}
+
+//   ngOnInit() {
+//     this.GetFamiliasArticulos();
+//   }
+
+//   GetFamiliasArticulos() {
+//     this.articulosFamiliasService.get()
+//     .subscribe((res:ArticuloFamilia[]) => {
+//       this.Items = res;
+//     });
+//   }
